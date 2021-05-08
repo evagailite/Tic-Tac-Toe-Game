686e4dab9ac95df17c75357f2fc98d3accfe7e86
evagailite <eva.gailite@gmail.com>
2021-05-09 00:41:42 +0300

    Changed tables and queries

--- src/main/java/main/queries.sql
+++ src/main/java/main/queries.sql
@@ -1,13 +1,13 @@
 
---1st table for creating a username
-CREATE TABLE IF NOT EXISTS username (
+--table for creating a user
+CREATE TABLE IF NOT EXISTS users (
 id_username INTEGER AUTO_INCREMENT PRIMARY KEY,
 username TEXT NOT NULL,
 name TEXT NOT NULL,
 age INTEGER
 );
 
---2nd table for game results
+--table for game results, consist of all possible ending
 CREATE TABLE IF NOT EXISTS results (
 id_results INTEGER AUTO_INCREMENT PRIMARY KEY,
 win INTEGER,
@@ -15,30 +15,82 @@ loose INTEGER,
 tie INTEGER
 );
 
---3rd table for player 1 - user
+CREATE TABLE IF NOT EXISTS moves (
+id_moves INTEGER AUTO_INCREMENT PRIMARY KEY,
+player_positions INTEGER,
+pcu_positions INTEGER,
+);
+
+CREATE TABLE IF NOT EXISTS game (
+id_user INTEGER AUTO_INCREMENT PRIMARY KEY,
+player_1 INTEGER,
+foreign key (player_1) REFERENCES player1(id_user)
+player_2 INTEGER,
+foreign key (player_2) REFERENCES playerPcu(id_pcu),
+player_1_score INTEGER,
+foreign key (player_1_score) REFERENCES results(id_results),
+player_2_score INTEGER,
+foreign key (player_2_score) REFERENCES results(id_results)
+);
+
+--table for player1 - user
 CREATE TABLE IF NOT EXISTS player1 (
 id_user INTEGER AUTO_INCREMENT PRIMARY KEY,
 player_username BIGINT,
-foreign key (player_username) REFERENCES username(id_username),
-player_moves INTEGER,
+foreign key (player_username) REFERENCES users(id_username),
+player_moves BIGINT,
+foreign key (player_moves) REFERENCES moves(id_moves),
 player_score BIGINT,
-foreign key (player_score) REFERENCES results(id_results)
+foreign key (player_score) REFERENCES game(id_user)
 );
 
---4th table for player 2 - pcu
+--table for player2 - pcu
 CREATE TABLE IF NOT EXISTS playerPcu (
 id_pcu INTEGER AUTO_INCREMENT PRIMARY KEY,
-pcu_moves INTEGER,
+pcu_moves BIGINT,
+foreign key (pcu_moves) REFERENCES moves(id_moves),
 pcu_opponent BIGINT,
-foreign key (pcu_opponent) REFERENCES username(id_username),
+foreign key (pcu_opponent) REFERENCES users(id_username),
 pcu_score BIGINT,
-foreign key (pcu_score) REFERENCES results(id_results)
+foreign key (pcu_score) REFERENCES game(id_user)
 );
+
+--table played games
+CREATE TABLE IF NOT EXISTS playedGames (
+id_played_game INTEGER AUTO_INCREMENT PRIMARY KEY,
+player_one INTEGER,
+foreign key (player_one) REFERENCES username(id_username),
+player_two INTEGER,
+foreign key (player_two) REFERENCES playerPcu(id_pcu),
+player_one_moves INTEGER,
+foreign key (player_one_moves) REFERENCES moves(id_moves),
+player_two_moves INTEGER,
+foreign key (player_two_moves) REFERENCES moves(id_moves),
+);
+
+-------------------------------------------
+--add data about username
+INSERT INTO username
+(username, name, age) VALUES
+(?, ?, ?);
 
 --add constance for game results
 INSERT INTO results
 (win, loose, tie) VALUES
 (2, 0, 1);
+
+--add player1 position
+INSERT INTO moves
+(player_positions) VALUES
+(?);
+
+--add pcu positions
+INSERT INTO moves
+(pcu_positions) VALUES
+(?);
+
+------------------------------------------
+
 
 --display username table
 SELECT * FROM username;
@@ -49,37 +101,9 @@ SELECT * FROM player1;
 --display cpu table
 SELECT * FROM cpu;
 
---add data about username
-INSERT INTO username
-(username, name, age) VALUES
-(?, ?, ?);
 
---add player1 moves
-INSERT INTO player1
-(moves) VALUES
-(?);
 
---add cpu moves
-INSERT INTO cpu
-(moves) VALUES
-(?);
 
---add player1 score
-INSERT INTO player1
-(score) VALUES
-(?);
-
---add cpu score
-INSERT INTO cpu
-(score) VALUES
-(?);
-
---add opponent for cpu
-INSERT INTO playerPcu
-(pcu_opponent) VALUES
-(?);
-
--- display player rank(scores)
 
 -- display all games played by specific player
 SELECT * FROM player1 WHERE username = ?;
