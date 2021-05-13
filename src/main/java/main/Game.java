8a621b7d2fa183a795626dc83c92fbfce6c6992d
evagailite <eva.gailite@gmail.com>
2021-05-13 23:02:54 +0300

    Game updated.

---
+++ src/main/java/main/Game.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Game {
+
+    private String result;
+
+    public Game() {
+        this.result = null;
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+
+}
